fe95ef3e52fd1d74801bd4a9eeec87f4a8f891dd
alinabezux <alinabezux@gmail.com>
2022-12-23 17:29:53 +0200

    5

--- configs/configs.js
+++ configs/configs.js
@@ -1,5 +1,4 @@
 module.exports = {
     PORT: process.env.PORT || 5000,
     DB_PASSWORD: process.env.DB_PASSWORD || '1284978',
-    MONGO_URL: process.env.MONGO_URL || 'mongodb://localhost:27017/test'
-}
+    MONGO_URL: process.env.MONGO_URL || 'mongodb://localhost:27017/test'}
