ec292854691df5808e1a02737eb90ae863438841
alinabezux <alinabezux@gmail.com>
2022-12-24 13:48:28 +0200

    5

--- DataBase/OAuth.js
+++ DataBase/OAuth.js
@@ -3,8 +3,7 @@ const {Schema, model} = require('mongoose');
 const OAuthSchema = new Schema({
     _user_id: {type: Schema.Types.ObjectId, ref: 'User'},
     accessToken: {type: String},
-    refreshToken: {type: String},
-
+    refreshToken: {type: String}
 }, {
     timestamps: true
 })
