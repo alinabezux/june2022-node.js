ad8a6ffd082df9274008396f83bd22b5ab88ec22
alinabezux <alinabezux@gmail.com>
2023-02-13 03:13:31 +0200

    sockets

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
+const http = require('http');       //робить сервер не на експресі,а на хттп (це потрібно для сокетів)
+const socketIO = require('socket.io');
 const express = require('express');
 const swaggerUI = require('swagger-ui-express');
 const fileUpload = require('express-fileupload');
@@ -12,11 +14,54 @@ const {cronRunner} = require("./cron");
 const swaggerJSON = require("./swagger.json");
 
 const app = express();
+const server = http.createServer(app);      //будуємо сервер на основі апп
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 app.use(fileUpload());
+
+const io = socketIO(server, {cors: 'http://localhost:80'});    //конекшн на бекенді,інформація про весь socket(всіх клієнтів)
+
+io.on('connection', (socket) => {   //івент connection ,який спрацьовує тоді,коли хтось приєднався до серверу,
+    console.log(socket);
+    socket.on('disconnect', () => {
+        console.log(`Socket ${socket.id} was disconnected`)
+    })
+
+    console.log(socket.id);     //socket-інформація про користувача
+    console.log(socket.handshake.auth);
+    console.log(socket.handshake.query.page);
+
+    socket.on('message:send', (data) => {
+        console.log(data.text);
+
+        //відправити подію всім підключеним
+        socket.emit('message:new', data.text);
+
+        //відправити подію всім,крім відправника
+        socket.broadcast.emit('message:new', data.text);
+
+        //відправити подію всім
+        io.emit('message:new', data.text);
+
+    })
+
+    socket.on('room:join', (roomInfo) => {
+        socket.join(roomInfo.roomId)        //socket join room
+        // socket.leave(roomInfo.roomId)       //socket leave room
+
+        //send to all on room except new member
+        // socket.to(roomInfo.roomId).emit('user:room:join', socket.id);
+
+        //send to all room members
+        io.to(roomInfo.roomId).emit('user:room:join', socket.id);
+
+    })
+});
+
+
+
 
 app.use('/auth', authRouter);
 app.use('/users', userRouter);
@@ -33,7 +78,7 @@ app.use((err, req, res, next) => {
     });
 });
 
-app.listen(configs.PORT, async () => {
+server.listen(configs.PORT, async () => {       //сервер слухає порт
     await mongoose.connect('mongodb://127.0.0.1:27017/june2022');
     console.log(`Server listen ${configs.PORT}`);
     // cronRunner();
